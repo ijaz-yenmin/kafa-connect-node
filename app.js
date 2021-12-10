d898ec5cb1a4978327a3786c1d41ff9ca6aa0d7f
ijaz-yenmin <ijaz.yenmin@gmail.com>
2021-12-10 18:57:54 +0530

    update list

--- app.js
+++ app.js
@@ -12,17 +12,13 @@ var async = require("async");
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(express.json());
-mongoose.connect("mongodb://34.93.71.234:27017/testdb?replicaSet=rs0", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 var kafka = require("kafka-node");
 var Consumer = kafka.Consumer,
   client = new kafka.KafkaClient("localhost:9092"),
   consumer = new Consumer(
     client,
-    [{ topic: "mongo1.testdb.users", partition: 0 }],
+    [{ topic: "mongo-connect.proton_dev.users", partition: 0 }],
     {
       autoCommit: true,
     }
@@ -38,8 +34,28 @@ io.on("connection", (socket) => {
 
 var socketMap = [];
 consumer.on("message", function (message) {
-  dataUpdate();
+  sendGetRequest();
 });
+
+const sendGetRequest = async () => {
+  try {
+    const resp = await axios.get(
+      "http://34.93.111.74:8080/aw-watcher-timeline",
+      {
+        headers: {
+          authorization: "Basic YWRtaW46c2VjcmV0",
+        },
+      }
+    );
+
+    console.log(resp.data);
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+};
+
+sendGetRequest();
 
 async function dataUpdate() {
   console.log("Socket Emmit");
