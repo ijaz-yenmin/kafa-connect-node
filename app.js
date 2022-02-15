6715587bb747de27350c4dea901a8b040721fac4
ijaz-yenmin <ijaz.yenmin@gmail.com>
2022-02-15 15:39:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ try {
         io.sockets.emit("overview-dashboard", data);
         io.sockets.emit("overview-activities", data);
         io.sockets.emit("youtube-activities", data);
+        io.sockets.emit("tree-activities", data);
         io.sockets.emit("activies-app", data);
         io.sockets.emit("dashboard-afk-app", data);
         io.sockets.emit("top-perform-app", data);
